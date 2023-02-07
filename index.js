1c6b1c5dc153660bb376c6d9a1bdfa4539def8b3
Ahmad <k_han21@yahoo.com>
2023-02-07 21:45:37 +1100

    added mongo uri from env

--- index.js
+++ index.js
@@ -12,13 +12,10 @@ const limiter = rateLimiter({
 
 mongoose.set('strictQuery', false)
 mongoose
-  .connect(
-    'mongodb+srv://amadywr:amadywr@merncluster.sbby1.mongodb.net/JOKESAPI?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err))
 
