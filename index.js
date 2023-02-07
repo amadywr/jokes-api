dda2b47f4a85b10034499301205ca3b1908340b7
Ahmad <k_han21@yahoo.com>
2023-02-07 22:09:56 +1100

    a

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ const limiter = rateLimiter({
 
 mongoose.set('strictQuery', false)
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    'mongodb+srv://amadywr:amadywr@merncluster.sbby1.mongodb.net/JOKESAPI?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err))
 
